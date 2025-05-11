e1083ff66674f19d9cec28535df5947b95373e77
whisky81 <anh666666anh@gmail.com>
2025-05-11 14:14:09 +0700

    get data of mongodb from fetchModelData.js

--- src/lib/fetchModelData.js
+++ src/lib/fetchModelData.js
@@ -4,8 +4,23 @@
  * @param {string} url      The URL to issue the GET request.
  *
  */
-function fetchModel(url) {
+async function fetchModel(url) {
   const models = null;
+  try {
+    const response = await fetch(`http://localhost:8081${url}`);
+    let ok = response.ok;
+    const data = await response.json(); 
+
+    if (!ok) {
+      throw new Error(data);
+    }
+
+    models = data; 
+  } catch (error) {
+    models = {
+      message: error.message 
+    }
+  }
   return models;
 }
 
