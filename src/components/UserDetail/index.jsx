e979aab5a6ca773544c2dedfaa0d6f432c29d4ca
whisky81 <anh666666anh@gmail.com>
2025-05-11 09:46:16 +0700

    update the UserDetail component

--- src/components/UserDetail/index.jsx
+++ src/components/UserDetail/index.jsx
@@ -1,30 +1,68 @@
 import React from "react";
-import {Typography} from "@mui/material";
-
-import "./styles.css";
-import {useParams} from "react-router-dom";
+import { Typography, Box, Divider, ImageList, ImageListItem } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import models from "../../modelData/models";
-import { Link } from 'react-router-dom'; 
+import "./styles.css";
+
 /**
- * Define UserDetail, a React component of Project 4.
+ * UserDetail component displays detailed information about a user.
  */
 function UserDetail() {
-    const user = useParams();
-    const userDetail = models.userModel(user.userId);
-    return (
-        <>
-          <Typography variant="body1">
-            <div>
-              <h2>{`${userDetail.first_name} ${userDetail.last_name}`}</h2>
-              <p>{userDetail.description}</p>
-              <p>Live: {userDetail.location}</p>
-            </div>
-            <div>
-              <h3><Link to={`/photos/${user.userId}`}>Gallery</Link></h3>
-            </div>
-          </Typography>
-        </>
-    );
+  const { userId } = useParams();
+  const userDetail = models.userModel(userId);
+  const photos = models.photoOfUserModel(userId);
+
+  if (!userDetail) {
+    return <Typography variant="body1">User not found</Typography>;
+  }
+
+  return (
+    <Box sx={{ p: 2 }}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        {`${userDetail.first_name} ${userDetail.last_name}`}
+      </Typography>
+
+      {userDetail.description && (
+        <Typography variant="body1" paragraph sx={{ fontStyle: 'italic' }}>
+          {userDetail.description}
+        </Typography>
+      )}
+
+      <Typography variant="body1" paragraph>
+        Live: {userDetail.location}
+      </Typography>
+
+      <Divider sx={{ my: 2 }} />
+
+      <Typography variant="h6" component="h2">
+        <Link
+          to={`/photos/${userId}`}
+          style={{
+            textDecoration: 'none',
+            color: 'inherit',
+            '&:hover': {
+              textDecoration: 'underline'
+            }
+          }}
+        >
+          Gallery
+        </Link>
+      </Typography>
+      <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+        {photos && photos.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              srcSet={`/images/${item.file_name}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              src={`/images/${item.file_name}?w=164&h=164&fit=crop&auto=format`}
+              alt={item.date_time}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Box>
+  );
 }
 
 export default UserDetail;
